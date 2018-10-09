32168ad9e8b33c76f1285866e190953fc0447795
carajean <ccpianist@gmail.com>
2018-10-09 16:25:41 -0700

    completed first two

--- foundations/assessment/01_objects/objects.js
+++ foundations/assessment/01_objects/objects.js
@@ -1,33 +1,70 @@
-/**
- * Vehicle Constructor
- * @param {[type]} licensePlate [description]
- * @param {[type]} color        [description]
- */
-var Vehicle = function(licensePlate, color) {
+function Vehicle (licensePlate, color) {
 	this.licensePlate = licensePlate;
 	this.color = color;
-};
+}
+Vehicle.prototype.beep = function(){
+	return 'BEEP, BEEP';
+}
+Vehicle.prototype.changeColor = function(){
+	this.color = 'red';
+}
 
-/**
- * the beep function should return the string 'BEEP, BEEP'
- * @return {[type]} [description]
- */
-Vehicle.prototype.beep = function() {
-	return "BEEP, BEEP";
-};
+let vehicle = new Vehicle(22589, 'blue');
+console.log(vehicle);
 
-Vehicle.prototype.changeColor = function(newColor) {
-	this.color = newColor;
-};
-
-var Truck = function(licensePlate, color, transmission) {
-	// Vehicle.call(this, licensePlate);
-	// Vehicle.call(this, color);
-	// this.licensePlate = licensePlate;
-	// this.color = color;
-	Vehicle.apply(this, [licensePlate, color]);
+function Truck(licensePlate, color, transmission){
+	Vehicle.call(this, licensePlate, color);
 	this.transmission = transmission;
-};
-
-Truck.prototype = Object.create(Vehicle.prototype);
+}
+Truck.prototype = Object.create(Vehicle);
 Truck.prototype.constructor = Truck;
+
+let truck = new Truck(47892, 'red', 'manual');
+console.log(truck);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * Vehicle Constructor
+//  * @param {[type]} licensePlate [description]
+//  * @param {[type]} color        [description]
+//  */
+// var Vehicle = function(licensePlate, color) {
+// 	this.licensePlate = licensePlate;
+// 	this.color = color;
+// };
+
+// /**
+//  * the beep function should return the string 'BEEP, BEEP'
+//  * @return {[type]} [description]
+//  */
+// Vehicle.prototype.beep = function() {
+// 	return "BEEP, BEEP";
+// };
+
+// Vehicle.prototype.changeColor = function(newColor) {
+// 	this.color = newColor;
+// };
+
+// var Truck = function(licensePlate, color, transmission) {
+// 	// Vehicle.call(this, licensePlate);
+// 	// Vehicle.call(this, color);
+// 	// this.licensePlate = licensePlate;
+// 	// this.color = color;
+// 	Vehicle.apply(this, [licensePlate, color]);
+// 	this.transmission = transmission;
+// };
+
+// Truck.prototype = Object.create(Vehicle.prototype);
+// Truck.prototype.constructor = Truck;
